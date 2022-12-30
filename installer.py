bf55bc03b158e3ba83264c075e0e90f9f1a19e6c
Harxi <90150354+Harxi@users.noreply.github.com>
2022-12-30 22:17:04 +0300

    Update installer.py

--- installer.py
+++ installer.py
@@ -2,7 +2,28 @@ import os
 
 if "bin" not in os.listdir():
 	os.mkdir("bin")
-
+	os.chdir("bin")
+	with open("commands.json", "w") as f:
+		f.write("""{
+	"echo": {
+		"filename": "echo.py",
+		"permission": 1,
+		"package": "standard"
+	}
+}
+""")
+	with open("echo.py", "w") as f:
+		f.write("""if len(args) > 1:
+	print(TMA())
+else:
+	if "h" in args or "help" in args:
+		print('Usage: echo [string]\\n    -h/--help - show this menu')
+	elif len(args) < 1:
+		print()
+	else:
+		print(args['0'])""")
+	os.chdir("..")
+		
 if "config" not in os.listdir():
 	os.mkdir("config")
 	os.chdir("config")
